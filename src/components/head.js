4ccbcaf8388bf29a89a7b104378a5dc23c7bbe45
= <vicjuma945@gmail.com>
2020-05-31 08:11:47 +0300

    debugging

--- src/components/head.js
+++ src/components/head.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import config from '@config';
 import favicon from '@images/favicons/favicon.ico';
-import ogImage from '@images/og.png';
 import appleIcon57x57 from '@images/favicons/apple-icon-57x57.png';
 import appleIcon60x60 from '@images/favicons/apple-icon-60x60.png';
 import appleIcon72x72 from '@images/favicons/apple-icon-72x72.png';
@@ -35,21 +34,18 @@ const Head = ({ metadata }) => (
     <meta property="og:type" content="website" />
     <meta property="og:url" content={metadata.siteUrl} />
     <meta property="og:site_name" content={metadata.title} />
-    <meta property="og:image" content={`${config.siteUrl}${ogImage}`} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
     <meta property="og:image:type" content="image/png" />
     <meta property="og:locale" content={config.siteLanguage} />
     <meta itemProp="name" content={metadata.title} />
     <meta itemProp="description" content={metadata.description} />
-    <meta itemProp="image" content={`${config.siteUrl}${ogImage}`} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:url" content={metadata.siteUrl} />
     <meta name="twitter:site" content={config.twitterHandle} />
     <meta name="twitter:creator" content={config.twitterHandle} />
     <meta name="twitter:title" content={metadata.title} />
     <meta name="twitter:description" content={metadata.description} />
-    <meta name="twitter:image" content={`${config.siteUrl}${ogImage}`} />
     <meta name="twitter:image:alt" content={metadata.title} />
 
     <link rel="apple-touch-icon" sizes="57x57" href={appleIcon57x57} />
@@ -68,7 +64,10 @@ const Head = ({ metadata }) => (
     <meta name="msapplication-TileColor" content={config.colors.navy} />
     <meta name="msapplication-TileImage" content={msIcon144x144} />
     <meta name="theme-color" content={config.colors.navy} />
-    <script data-ad-client="ca-pub-2021383553629546" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script
+      data-ad-client="ca-pub-2021383553629546"
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   </Helmet>
 );
 
